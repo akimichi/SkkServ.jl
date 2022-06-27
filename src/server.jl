8c394bb6be18a6762dfea275a071742de76dc8db
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2022-06-27 13:53:32 +0900

    server.jlを追加

---
+++ src/server.jl
@@ -0,0 +1,35 @@
+#!/opt/julia/bin/julia
+
+#=
+    ソケットサーバ
+    1:1 のコネクションを複数接続可
+    Todo : シグナル見て停止とかタイムアウトとかサーバらしい実装が必要
+=#
+
+# ソケットを準備
+using Sockets
+using SkkServ
+
+
+server = listen(IPv4(0),1178)
+
+# 無限ループさせる
+while true
+    # 接続待ち
+    socket = accept(server)
+    # 接続が来たら、@asyncでコルーチン生成
+    # 接続をコピー
+    # peer = socket
+    @async while isopen(socket)
+        try
+          # 接続をコピー
+          # peer = socket
+          # リクエスト読込
+          write(sock, readline(socket, keep=true))
+      catch err
+        println("エラーです")
+        println(err)
+        exit()
+      end
+    end
+end
