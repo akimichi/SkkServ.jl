c2a81d8c68652d07f15df07f0fa6dcb5a3e2a7f8
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2022-07-25 14:53:15 +0900

    単体テストに着手

--- src/entry.jl
+++ src/entry.jl
@@ -1,6 +1,7 @@
 
 function entry(line)
-  command = line[1],
-  midashi = strip(line[2:end], [' ','\n'])
+  command = line[1]
+  midashi = strip(line[2:end])
+  # midashi = strip(line[2:end], [' ','\n'])
   return (command = command, midashi = midashi) 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,8 @@ using Test
 @testset "SkkServ.jl" begin
   @testset "entry" begin
       line = "1a \n"
+      @test line == "1a \n"
       tuple = entry(line)
-      @test tuple.command == "0"
+      @test tuple.command == '1'
   end
 end
